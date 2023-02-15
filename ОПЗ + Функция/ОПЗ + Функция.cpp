4a0c9662c3c85c926a2252fe913f50405a413d63
Roman <125295347+Zucewora@users.noreply.github.com>
2023-02-15 18:32:23 +0300

    Update ОПЗ + Функция.cpp

--- ОПЗ + Функция/ОПЗ + Функция.cpp
+++ ОПЗ + Функция/ОПЗ + Функция.cpp
@@ -1,6 +1,3 @@
-﻿// стек.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -10,7 +7,6 @@ using namespace std;
 #pragma warning (disable :4996)
 using namespace std;
 
-
 void Type(char StRing, char openBr, char closeBr, vector <char>& stackk, int& k) { // проверка на скобки 
     if (StRing == openBr) {
         stackk.push_back(StRing);
@@ -19,15 +15,12 @@ void Type(char StRing, char openBr, char closeBr, vector <char>& stackk, int& k)
     if (StRing == closeBr) {
         k--;
     }
-
     if (stackk.size() != 0) {
-
         if (StRing == closeBr) {
             if (stackk.size() != 0) {
                 if ((stackk[stackk.size() - 1] == '(') && (StRing == ')')) {
                     if (stackk.size() != 0) {
                         stackk.pop_back();
-
                     }
                 }
             }
@@ -35,7 +28,6 @@ void Type(char StRing, char openBr, char closeBr, vector <char>& stackk, int& k)
                 if ((stackk[stackk.size() - 1] == '{') && (StRing == '}')) {
                     if (stackk.size() != 0) {
                         stackk.pop_back();
-
                     }
                 }
             }
@@ -43,7 +35,6 @@ void Type(char StRing, char openBr, char closeBr, vector <char>& stackk, int& k)
                 if ((stackk[stackk.size() - 1] == '[') && (StRing == ']')) {
                     if (stackk.size() != 0) {
                         stackk.pop_back();
-
                     }
                 }
             }
@@ -51,34 +42,22 @@ void Type(char StRing, char openBr, char closeBr, vector <char>& stackk, int& k)
     }
 }
 
-
-
 double Res(string OPZ, double& x, bool& success,double &input_x) {
-
     vector <int> stack_number;
     vector <char> stack_func;
     
-
     for (int i = 0; i < OPZ.size(); i++) {
         if (OPZ[i] == 'x' ) {
-
             OPZ[i] = input_x+'0';
         }
     }
-
-
     for (int i = 0; i < OPZ.size(); i++) {
-        
-
         if (OPZ[i] >= '0' && OPZ[i] <= '9') {
-
             stack_number.push_back(OPZ[i]);
         }
-
         if (OPZ[i] >= '*' && OPZ[i] < '0') {
             if (stack_number.size() > 1) {
                 switch (OPZ[i]) {
-
                 case '+': {
                     double x1 = (stack_number[stack_number.size() - 1] - '0');
                     double x2 = (stack_number[stack_number.size() - 2] - '0');
@@ -109,22 +88,15 @@ double Res(string OPZ, double& x, bool& success,double &input_x) {
                     }
                     break;
                 }
-
-
-
                 }
-
-
                 stack_number.pop_back();
                 stack_number.pop_back();
                 stack_number.push_back(x+'0');
             }
         }
-        //cout << stack_number[stack_number.size() - 1] << endl;
     }
     return x;
 }
-
 
 int main()
 {
@@ -135,18 +107,13 @@ int main()
     int k = 0;
     double x = 0;
     bool success = 1;
-
     vector <char> stack;
-    
-
     string OpenBracketsList = { '(' , '{' , '[' };
     string CloseBracketsList = { ')' , '}' , ']' };
     string out_string;
     string STRING;
     
     cin >> STRING;
-     
-
     if (1) { //Вводная проверка данных 
         for (int i = 0; i < STRING.size(); i++) {
             for (int d = 0; d < 3; d++) {
@@ -167,18 +134,14 @@ int main()
             }
         }
     }
-
-
     
     for (int i = 0; i < STRING.size(); i++) { // работа стека 
         //cout << out_string << endl;
         if ((STRING[i] >= '0') && (STRING[i] <= '9') || (STRING[i] == 'x')) { // если число то записываем в выходную строку
             out_string.push_back(STRING[i]);
-            
         }
 
         if ((STRING[i] == '(') || (STRING[i] == '*') || (STRING[i] == '{') || (STRING[i] == '[') || (STRING[i] == '^') || (STRING[i] == '/') || (STRING[i] == '+') || (STRING[i] == '-')) {
-
             if ((STRING[i] == '+') || (STRING[i] == '-')) {
                 if (stack.size() >= 1) {
                     if ((stack[stack.size() - 1] == '-') || (stack[stack.size() - 1] == '+') ||
@@ -187,9 +150,7 @@ int main()
                         stack.pop_back();
                     }
                 }
-
             }
-
             if ((STRING[i] == '*') || (STRING[i] == '/')) {
                 if (stack.size() >= 1) {
                     if ((stack[stack.size() - 1] == '*') || (stack[stack.size() - 1] == '/')) {
@@ -198,13 +159,11 @@ int main()
                     }
                 }
             }
-
             if (stack.size() != 0) {
                 if ((stack[stack.size() - 1] == '+') && (STRING[i] == '+')) {
                     out_string.push_back(stack[stack.size() - 1]);
                     stack.pop_back();
                 }
-
             }
             if (stack.size() != 0) {
                 if ((stack[stack.size() - 1] == '-') && (STRING[i] == '-')) {
@@ -212,10 +171,8 @@ int main()
                     stack.pop_back();
                 }
             }
-
             stack.push_back(STRING[i]);
         }
-
         if ((STRING[i] == ')') || (STRING[i] == '}') || (STRING[i] == ']')) {
             if (stack.size() != 0) {
                 while ((stack[stack.size() - 1] != '(') ) {
@@ -237,18 +194,14 @@ int main()
 
 
         if (i + 1 == STRING.size()) {
-
             while (stack.size() != 0) {
                 out_string.push_back(stack[stack.size() - 1]);
-
                 stack.pop_back();
             }
         }
     } // опз
 
-
     
-
     cout << "Итог ОПЗ: " << out_string << endl;
     
     double min_input_x = 0;
@@ -261,11 +214,8 @@ int main()
             cout << min_input_x << "     " << Res(out_string, x, success, min_input_x) << endl;
             if (min_input_x + h > max_input_x) {
                 h = max_input_x - min_input_x;
-                
             }
             min_input_x += h;
-
-
         } while (min_input_x != max_input_x);
     }
     else {
